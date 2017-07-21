75d4ba400966646a98be490fad23c0844a985ac8
kylepassman <herbiedoo@Herbies-MacBook-Pro.local>
2017-07-21 14:16:15 -0400

    bunch of changes

---
+++ KMtestJS.js
@@ -0,0 +1,18 @@
+document.querySelector(".white").addEventListener("click", function(){
+  document.querySelector('.colorBox').style.backgroundColor = "white";
+});
+document.querySelector(".orange").addEventListener("click", function(){
+  document.querySelector('.colorBox').style.backgroundColor = "orange";
+});
+document.querySelector(".green").addEventListener("click", function(){
+  document.querySelector('.colorBox').style.backgroundColor = "green";
+});
+document.querySelector(".blue").addEventListener("click", function(){
+  document.querySelector('.colorBox').style.backgroundColor = "blue";
+});
+document.querySelector(".yellow").addEventListener("click", function(){
+  document.querySelector('.colorBox').style.backgroundColor = "yellow";
+});
+document.querySelector(".red").addEventListener("click", function(){
+  document.querySelector('.colorBox').style.backgroundColor = "red";
+});
